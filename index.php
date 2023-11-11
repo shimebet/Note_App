bdac64c7e85957c86f688ee1f8057ca036e09d89
techane1213 <shimetechane@gmail.com>
2023-11-11 20:44:52 +0300

    add validation

--- index.php
+++ index.php
@@ -2,38 +2,39 @@
 include('conn/conn.php');
 session_start();
 
-// When form submitted, check and create user session.
-if (isset($_POST['username'])) {
-    $username = stripslashes($_POST['username']); // removes backslashes
-    $username = htmlspecialchars($username); // convert special characters to HTML entities
-    $password = stripslashes($_POST['password']);
-    $password = htmlspecialchars($password);
-
-    // Check user is exist in the database
-    $query = "SELECT * FROM `users` WHERE username=:username AND password=:password";
-    $stmt = $conn->prepare($query);
-
-    // Bind parameters
-    $stmt->bindParam(':username', $username);
-    $stmt->bindParam(':password', md5($password));
-
-    // Execute the query
-    $stmt->execute();
-
-    // Fetch the result
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($result) {
-        $_SESSION['username'] = $username;
-        // Redirect to user dashboard page
-        header("Location: index2.php");
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (empty($_POST['username']) || empty($_POST['password'])) {
+        echo "<script>alert('Username and password are required. Please enter both.');</script>";
     } else {
-        echo "<div class='form'>
-              <h3>Incorrect Username/password.</h3><br/>
-              <p class='link'>Click here to <a href='index.php'>Login</a> again.</p>
-              </div>";
+        $username = stripslashes($_POST['username']);
+        $username = htmlspecialchars($username);
+        $password = stripslashes($_POST['password']);
+        $password = htmlspecialchars($password);
+
+        // Check user is exist in the database
+        $query = "SELECT * FROM `users` WHERE username=:username AND password=:password";
+        $stmt = $conn->prepare($query);
+
+        // Bind parameters
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', md5($password));
+
+        // Execute the query
+        $stmt->execute();
+
+        // Fetch the result
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($result) {
+            $_SESSION['username'] = $username;
+            // Redirect to user dashboard page
+            header("Location: index2.php");
+            exit(); // Ensure no further code is executed after the redirect
+        } else {
+            echo "<script>alert('Incorrect Username or Password. Please enter correct credentials.');</script>";
+        }
     }
-} else {
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -48,10 +49,8 @@ if (isset($_POST['username'])) {
         <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
         <input type="password" class="login-input" name="password" placeholder="Password"/>
         <input type="submit" value="Login" name="submit" class="login-button"/>
-        <p class="link">Don't have an account? <a href="registration.php">Registration Now</a></p>
+        <p class="link">Don't have an account? <a href="registration.php">Register Now</a></p>
     </form>
 </body>
 </html>
-<?php
-}
-?>
+ 
--- registration.php
+++ registration.php
@@ -15,56 +15,68 @@
     </style>
 </head>
 <body>
+
 <?php
 include('conn/conn.php');
 
-// When form submitted, insert values into the database.
-if (isset($_POST['username'])) {
-    // removes backslashes
-    $username = stripslashes($_POST['username']);
-    // escapes special characters in a string
-    $username = htmlspecialchars($username);
-    $email    = stripslashes($_POST['email']);
-    $email    = htmlspecialchars($email);
-    $password = stripslashes($_POST['password']);
-    $password = htmlspecialchars($password);
-    $create_datetime = date("Y-m-d H:i:s");
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $username = trim($_POST['username']);
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
 
-    // Use prepared statements to prevent SQL injection
-    $query = "INSERT INTO users (username, password, email, create_datetime) VALUES (:username, :password, :email, :create_datetime)";
-    $stmt = $conn->prepare($query);
+    // Validate username
+    if (strlen($username) < 4 || strlen($username) > 15) {
+        echo "<p class='error-popup'>Username must be between 4 and 15 characters.</p>";
+    }
+    // Validate password
+    elseif (strlen($password) <= 4 || !preg_match('/^(?=.*[a-zA-Z])(?=.*\d).+$/', $password)) {
+        echo "<p class='error-popup'>Password must be greater than 4 characters and contain a combination of letters and numbers.</p>";
+    }
+    // Validate email existence
+    elseif (emailExists($conn, $email)) {
+        echo "<p class='error-popup'>An account with this email already exists. Please login or use a different email.</p>";
+    } else {
+        // Prepared statement to prevent SQL injection
+        $query = "INSERT INTO users (username, password, email, create_datetime) VALUES (:username, :password, :email, NOW())";
+        $stmt = $conn->prepare($query);
 
-    // Bind parameters
-    $stmt->bindParam(':username', $username);
-    $stmt->bindParam(':password', md5($password));
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':create_datetime', $create_datetime);
+        // Bind parameters
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', md5($password));
+        $stmt->bindParam(':email', $email);
 
-    // Execute the query
-    $result = $stmt->execute();
+        // Execute the query
+        $result = $stmt->execute();
 
-    // Display a popup message based on the result
-    echo "<script>
+        // Display a popup message based on the result
+        echo "<script>
             if ($result) {
                 alert('Registration successful');
                 window.location.href = 'registration.php';
             } else {
-                alert('Registration failed. Required fields are missing.');
-                window.location.href = 'registration.php';
+                alert('Registration failed. Please check your input and try again.');
             }
           </script>";
-} else {
-?>
-    <form class="form" action="" method="post">
-        <h1 class="login-title">Registration</h1>
-        <input type="text" class="login-input" name="username" placeholder="Username" required />
-        <input type="text" class="login-input" name="email" placeholder="Email Address">
-        <input type="password" class="login-input" name="password" placeholder="Password">
-        <input type="submit" name="submit" value="Register" class="login-button">
-        <p class="link">Already have an account? <a href="index.php">Login here</a></p>
-    </form>
-<?php
+    }
+}
+
+function emailExists($conn, $email) {
+    $query = "SELECT * FROM users WHERE email = :email";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC) !== false;
 }
 ?>
+
+<form class="form" action="" method="post">
+    <h1 class="login-title">Registration</h1>
+    <input type="text" class="login-input" name="username" placeholder="Username" required />
+    <input type="text" class="login-input" name="email" placeholder="Email Address">
+    <input type="password" class="login-input" name="password" placeholder="Password">
+    <input type="submit" name="submit" value="Register" class="login-button">
+    <p class="link">Already have an account? <a href="index.php">Login here</a></p>
+</form>
+
 </body>
 </html>
